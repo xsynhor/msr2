91189070e3bb96e858d9dfd6d3e3e48a7cdd4365
xsynhor <xsynhor@mailfence.com>
2025-01-23 08:51:02 -0500

    Added Calculate Area

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,10 +1,10 @@
 let lenght;
 let width;
 
-function calcualteArea() {
+function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
 
     let area = length * width;
-    document.getElementById('result').innerText = 'The area of the rectangle is: ${area}';
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
