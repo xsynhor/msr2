646e7b68aa694fec4d4368b5fbb4ac13c7855360
xsynhor <xsynhor@mailfence.com>
2025-01-27 06:47:52 -0500

    Added Employee Details

---
+++ employeeDetails/newFile.js
@@ -0,0 +1,4 @@
+{
+    const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML = totalEmployees;
+}
